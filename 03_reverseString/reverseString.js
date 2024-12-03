8569eac9a35fb14216ab57a781600315adaac0b4
amengelkamp <aliciamengelkamp@icloud.com>
2024-12-03 21:22:09 +0100

    first test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
 const reverseString = function() {
 
-};
+    let string = "hello";
+    let splitstring = string.split("");
+    let reverseString = splitstring.reverse().join(''); 
 
+return reverseString;
+};
 // Do not edit below this line
 module.exports = reverseString;
