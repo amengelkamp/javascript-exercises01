b7e48151e4a8e9afa233333f5acef621cd0b4927
amengelkamp <aliciamengelkamp@icloud.com>
2024-12-03 06:40:55 +0100

    02 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
+let greeting = "hey";
+let threegreetings = "";
 const repeatString = function() {
 
+let i = 0;
+
+for (i = 0; i < 3; i++) { 
+  threegreetings = threegreetings + greeting;
+}
+return threegreetings;
 };
 
 // Do not edit below this line
