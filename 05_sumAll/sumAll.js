116624f68804e441983addd19186ab1f2d094802
amengelkamp <aliciamengelkamp@icloud.com>
2024-12-06 13:06:17 +0100

    at least it counts

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(beginningnumber, endingnumber) {
+
+    let sumbetween = 0;
+    console.log("Die beginningnumber ist " +  beginningnumber);
+    console.log("Die endingnumber ist " + endingnumber);
+    for (let i = beginningnumber; i <= endingnumber; i++) { 
+        sumbetween = beginningnumber + 1;
+        console.log("Die sumbetween ist " + sumbetween);
+        console.log("i ist gleich " + (i));
+    }
+
+    const newSum = beginningnumber + sumbetween + endingnumber;
+
+    
+
+   
+
+
+    return newSum;
+
+    //sumAll(1, 4) returns the sum of 1 + 2 + 3 + 4 which is 10
+    //beginningnumber + beginningnumber+1 while <endingnumber + endingnumber
+
+    //sumAll(5,12) returns the sum of 5+6+7+8+9+10+11+12 = 68
+
+
 
 };
 
+console.log("Testcase 1 - sums numbers within the range: Die finale Summe lautet " + sumAll(1, 4));
+
+
 // Do not edit below this line
-module.exports = sumAll;
+//module.exports = sumAll;
