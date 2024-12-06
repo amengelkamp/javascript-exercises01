d2ef8b10eba780f98eac1ea2a97f469d264f40c4
amengelkamp <aliciamengelkamp@icloud.com>
2024-12-06 08:31:46 +0100

    first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    let numbers = [1, 2, 3, 4];
+    numbers = numbers.filter(number => number !== 3);
+
+    return numbers; // Outputs: [1, 2, 4, 5]
+ 
 };
 
 // Do not edit below this line
