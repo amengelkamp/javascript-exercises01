8ab2fb4f0687038161eedf3587feb81acfa067ae
amengelkamp <aliciamengelkamp@icloud.com>
2024-12-07 10:15:19 +0100

    3 test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,31 +1,25 @@
 const sumAll = function(beginningnumber, endingnumber) {
 
-    let sumbetween = 0;
-    console.log("Die beginningnumber ist " +  beginningnumber);
-    console.log("Die endingnumber ist " + endingnumber);
+    //console.log("Die beginningnumber ist " +  beginningnumber);
+    //console.log("Die endingnumber ist " + endingnumber);
+    //beginningnumber + beginningnumber+1 while <endingnumber + endingnumber
+    newSum = (0);
+    
+    if (beginningnumber > endingnumber) {
+        [beginningnumber, endingnumber] = [endingnumber, beginningnumber]; 
+    }
     for (let i = beginningnumber; i <= endingnumber; i++) { 
-        sumbetween = beginningnumber + i;
-        console.log("Die sumbetween ist " + sumbetween);
-        console.log("i ist gleich " + (i));
+        newSum += i;
+
+  
     }
 
-    const newSum = beginningnumber + sumbetween + endingnumber;
-
-    
-
-    return newSum;
-
-    //sumAll(1, 4) returns the sum of 1 + 2 + 3 + 4 which is 10
-    //beginningnumber + beginningnumber+1 while <endingnumber + endingnumber
-
-    //sumAll(5,12) returns the sum of 5+6+7+8+9+10+11+12 = 68
-
-
+     return newSum;
 
 };
 
-console.log("Testcase 1 - sums numbers within the range: Die finale Summe lautet " + sumAll(1, 4));
+console.log("Testcase 1 - sums numbers within the range: Die finale Summe lautet " + sumAll(123, 1));
 
 
 // Do not edit below this line
-//module.exports = sumAll;
+module.exports = sumAll;
